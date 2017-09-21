d1747f2943d41a394a999fed6ee14515d1803a72
J. Alexander <xandermoorman@gmail.com>
2017-09-20 22:24:09 -0500

    Adjusted code to deal with non-Scalaness of Spark

--- StringsHistogram.scala
+++ StringsHistogram.scala
@@ -1,85 +1,44 @@
 
-import scala.io.Source
-import java.io._
-import scala.annotation.tailrec
-
 /**
   * Program for Apache Spark with imports omitted because I used DataBricks.
   * Program creates a histogram of words discovered in Strings output from bulk_extractor.
   * I has many other uses though.
   */
 
-val newRDD = sc.textFile("/FileStore/tables/f10dnito1505888092349/file01.txt")
+val rdd = sc.textFile("/FileStore/tables/526w99d81505957985736/file01.txt")
+val rdd2 = sc.textFile("/FileStore/tables/526w99d81505957985736/file02.txt")
+val rdd3 = sc.textFile("/FileStore/tables/526w99d81505957985736/file03.txt")
+val rdd4 = sc.textFile("/FileStore/tables/526w99d81505957985736/file04.txt")
 
-val files = List("/FileStore/tables/f10dnito1505888092349/file02.txt",
-  "/FileStore/tables/f10dnito1505888092349/file03.txt",
-  "/FileStore/tables/f10dnito1505888092349/file04.txt",
-"/FileStore/tables/f10dnito1505888092349/file05.txt")
+val regex = "//d+-(HYPERFILE|ZIP|GZIP)-//d+//s+".r
 
-@tailrec
-def filterIt(newRdd: List[String], oldRdd: RDD): RDD ={
-  if (newRdd == None){
-    oldRDD
-  } else{
-    val currentRDD = sc.textFile(newRDD.head)
-    val regex = "//d+-(HYPERFILE|ZIP|GZIP)-//d+//s+".r
-
-    val rdd = currentRDD.map(x => regex.replaceAllIn(x, ""))
-   .map(x => regex3.replaceAllIn(x, "")).map(_.toLowerCase).map(_.trim)
-
-    val result = oldRdd.union(rdd)
-  }
-  filterIt( newRDD.tail, result)
-} // END filterAndUnion
-
-val fullRdd = filterIt(files, newRDD)
-
-val count = fullRdd.map(x => (x, 1)).reduceByKey(_ + _)
-val invert = count.map((x, y) -> (y, x)).sortByKey(ascending = false)
-
-// val vec = count.collect.toVector
-
-// val sorted = vec.sortWith(_._2 > _._2)
-
-val finalResult = invert.map(x => s"${x._2} - ${x._1}")
-
-finalResult.collect.toTextFile("SparkWindows10")
-// finalResult.take(50).foreach(println)
-
-
-/** SECTIONS FOR DEALING WITH SMALLER AMOUNTS OF DATA  */
-
-
-/** Alternative style program prints to file. */
-
-val list = Source.fromFile("wordlist.txt").getLines.toVector
-// Used to clean up bulk_extractor output
-val reg = "$\\d+(\\s+|".r
-
-// Over 7 million entries so process in parallel
-val cleanedList = list.par
-  .map(x => reg.replaceAllIn(x, ""))
+val clean1 = rdd.map(x => regex.replaceAllIn(x, ""))
   .map(_.toLowerCase)
   .map(_.trim)
 
-val rdd = sc.parallelize(cleanedList)
 
-// Pretty much groupBy combined with reduce function.
-val count = rdd.map(x => (x, 1)).reduceByKey(_ + _)
+val clean2 = rdd2.map(x => regex.replaceAllIn(x, ""))
+  .map(_.toLowerCase)
+  .map(_.trim)
 
-val vec = count.collect.toVector
+val result1 = clean1.union(clean2)
 
-// Sort big to small
-val sorted = vec.sortWith(_._2 > _._2)
+val clean3 = rdd3.map(x => regex.replaceAllIn(x, ""))
+  .map(_.toLowerCase)
+  .map(_.trim)
 
-// Make it print pretty
-val finalResult = sorted.map(x => s"${x._1} - ${x._2}")
+val result2 = result1.union(clean3)
 
-// Convert Vector to String
-val result = finalResult.mkString("\n")
+val clean4 =  rdd4.map(x => regex.replaceAllIn(x, ""))
+  .map(_.toLowerCase)
+  .map(_.trim)
 
-// Write to txt file
-val file = new File("histogram.txt")
-val bw = new BufferedWriter(new FileWriter(file))
-bw.write(result)
-bw.close()
+val result3 = result2.union(clean4)
+
+val fullRDD = result3.union(result3)
+
+val count = fullRDD.map(x => (x, 1)).reduceByKey(_ + _)
+val invert = count.map(_.swap).sortByKey(ascending = false)
+
+val finalResult = invert.map(x => s"${x._2} - ${x._1}")
+finalResult = saveAsTextFile("testProgram")
