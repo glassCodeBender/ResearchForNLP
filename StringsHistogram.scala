87eae9a89138916abb75e7c55a4e55b507259f7c
J. Alexander <xandermoorman@gmail.com>
2017-09-20 21:52:57 -0500

    Updated logic to deal with huge amounts of data. Untested.

--- StringsHistogram.scala
+++ StringsHistogram.scala
@@ -1,30 +1,60 @@
 
 import scala.io.Source
 import java.io._
+import scala.annotation.tailrec
 
 /**
-  * Program for Apache Spark with imports omitted
-  *
+  * Program for Apache Spark with imports omitted because I used DataBricks.
   * Program creates a histogram of words discovered in Strings output from bulk_extractor.
   * I has many other uses though.
   */
 
-val rdd = sc.textFile("/FileStore/tables/f10dnito1505888092349/texttest.txt")
+val newRDD = sc.textFile("/FileStore/tables/f10dnito1505888092349/file01.txt")
 
-val count = rdd.map(x => (x, 1)).reduceByKey(_ + _).sortByKey()
+val files = List("/FileStore/tables/f10dnito1505888092349/file02.txt",
+  "/FileStore/tables/f10dnito1505888092349/file03.txt",
+  "/FileStore/tables/f10dnito1505888092349/file04.txt",
+"/FileStore/tables/f10dnito1505888092349/file05.txt")
 
-val vec = count.collect.toVector
+@tailrec
+def filterIt(newRdd: List[String], oldRdd: RDD): RDD ={
+  if (newRdd == None){
+    oldRDD
+  } else{
+    val currentRDD = sc.textFile(newRDD.head)
+    val regex = "//d+-(HYPERFILE|ZIP|GZIP)-//d+//s+".r
 
-val sorted = vec.sortWith(_._2 > _._2)
+    val rdd = currentRDD.map(x => regex.replaceAllIn(x, ""))
+   .map(x => regex3.replaceAllIn(x, "")).map(_.toLowerCase).map(_.trim)
 
-val finalResult = sorted.map(x => s"${x._1} - ${x._2}")
-finalResult.foreach(println)
+    val result = oldRdd.union(rdd)
+  }
+  filterIt( newRDD.tail, result)
+} // END filterAndUnion
+
+val fullRdd = filterIt(files, newRDD)
+
+val count = fullRdd.map(x => (x, 1)).reduceByKey(_ + _)
+val invert = count.map((x, y) -> (y, x)).sortByKey(ascending = false)
+
+// val vec = count.collect.toVector
+
+// val sorted = vec.sortWith(_._2 > _._2)
+
+val finalResult = invert.map(x => s"${x._2} - ${x._1}")
+
+finalResult.collect.toTextFile("SparkWindows10")
+// finalResult.take(50).foreach(println)
+
+
+/** SECTIONS FOR DEALING WITH SMALLER AMOUNTS OF DATA  */
+
 
 /** Alternative style program prints to file. */
 
 val list = Source.fromFile("wordlist.txt").getLines.toVector
 // Used to clean up bulk_extractor output
-val reg = "$\\d+\\s+".r
+val reg = "$\\d+(\\s+|".r
 
 // Over 7 million entries so process in parallel
 val cleanedList = list.par
